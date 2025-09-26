3d11c8cef2176f203b729b3059bae0dac00b2214
abbykeck <abigailkeck2425@gmail.com>
2025-09-26 18:42:52 +0000

    finished fileIO program

--- fileIO.cpp
+++ fileIO.cpp
@@ -5,6 +5,7 @@
 int main() {
 	std::ifstream inFile;
 	std::stringstream converter;
+	std::stringstream ss;
 	std::string currentLine;
 	std::string sNum;
 	int num1;
@@ -13,6 +14,37 @@ int main() {
 	std::string text;
 	inFile.open("data.csv");
 	bool keepGoing = true;
+	while(keepGoing) {
+		getline(inFile, currentLine);
+		ss.clear();
+		ss.str(currentLine);
+		converter.clear();
+		converter.str("");
+		getline(ss, sNum, ',');
+		converter << sNum;
+		converter >> num1;
+		converter.clear();
+                converter.str("");
+		getline(ss, sNum, ',');
+		converter << sNum;
+		converter >> num2;
+		getline(ss, text);
+		num3 = (num1 + num2);
+		/*
+		std::cout << "num1: " << num1 << std::endl;
+		std::cout << "num2: " << num2 << std::endl;
+		std::cout << "num3: " << num3 << std::endl;
+		std::cout << "text: " << text << std::endl;
+		*/
+		if (inFile.eof()) {
+			keepGoing = false;
+		} else {
+			for (int i = 0; i < num3; i++) {
+                        	std::cout << text << " ";
+                	} // end for
+			std::cout << std::endl;
+		} // end if
+	} // end while
 	inFile.close();
 	return 0;
-}
+} // end main
